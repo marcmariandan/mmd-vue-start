d3ec6c00e48281f972107e1c4909544e7d83e8c8
marcmariandan <marcmariandan@gmail.com>
2022-06-10 07:38:53 +0300

    Add files via upload

--- createapp.js
+++ createapp.js
@@ -1,10 +1,31 @@
-
+  console.log('Script Started')
+  
+  /*
   const { createApp }  = Vue
   createApp({
     data() {
       return {
-        message: 'Salut Vue!'
+        message: 'Salutare Vue!'
       }
     }
-  }).mount('#app')
- 
+  }).mount('#myapp')
+ */
+
+  const myappcomponent ={
+    data(){
+      return {
+        cnt : 0
+      }
+    },
+    methods : {
+      increment(){
+        this.cnt += 1
+      },
+      reset(){
+        this.cnt = 0
+      }
+    }
+  }
+  const myapp = Vue.createApp(myappcomponent).mount('#myapp')
+
+  console.log('Script Repeted')
